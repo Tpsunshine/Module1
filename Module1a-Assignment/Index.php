769836bfa44adaaf4c2a86ada195b31f81fc1687
TP-Edureka <tpdawnsun@gmail.com>
2022-02-06 13:52:03 +0200

    Module 1

---
+++ Module1a-Assignment/Index.php
@@ -0,0 +1,8 @@
+<?php
+$x=100;
+$y=60;
+echo "The sum of x and y is : ". ($x+$y) . "<br />";
+echo "The difference between x and y is : ". ($x-$y) . "<br />";
+echo "The product of x and y : ". ($x*$y) . "<br />";
+echo "The Division of x and y : ". ($x/$y) . "<br />";
+?>
---
+++ Module1b-Assignment/Index.php
@@ -0,0 +1,17 @@
+<?php
+
+// First String
+$fname = 'John';
+
+// Second String
+$lname = 'Carter';
+
+//Concatenation Of String
+$c = $fname. "  ".$lname;
+
+// print Concatenate String
+echo " $c \n";
+
+?>
+
+ 
